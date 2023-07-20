f8d9d6a05bead85ee21e6a8b08b9d44385b95016
volkan <volkankanat.mdbf17@iste.edu.tr>
2023-07-20 14:13:57 +0300

    about and contact components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
