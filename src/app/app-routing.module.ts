f0a52ecb36f18dcceb9d8b2a7f288e9b6035c970
volkan <volkankanat.mdbf17@iste.edu.tr>
2023-07-20 14:23:39 +0300

    routes created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'about',
+    component: AboutComponent
+  },
+  {
+    path:'contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
